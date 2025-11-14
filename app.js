141231872f203a32de945bd46f4b2b8b1a2a2ac7
Ahmed6-7 <pirahmedmehmood786@gmail.com>
2025-11-14 11:59:04 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,70 @@
+//Q-01
+let userInput = prompt("Enter your Value:")
+console.log("Number : ", userInput);
+let roundValue = Math.round(userInput)
+console.log("Round Value : ", roundValue);
+
+let flooValue = Math.floor(userInput)
+console.log("Floor Value : ", flooValue);
+let ceilValue = Math.ceil(userInput)
+console.log("Ceil Value : ", ceilValue);
+console.log("Total Value:", roundValue + " " + flooValue + " " + ceilValue);
+
+//Q-02
+
+let inputUser = prompt("Enter your Negative Value:")
+console.log(inputUser);
+let valuRound = Math.round(inputUser)
+console.log("Negative Value of Round : ", valuRound);
+let valuFloor = Math.floor(inputUser)
+console.log("Negative Value of Floor : ", valuFloor);
+let valuCeil = Math.ceil(inputUser)
+console.log("Negative Value of Ceil : ", valuCeil);
+
+//Q-03
+let userAbsolute = -4.51
+console.log("Negative Numeber :", userAbsolute);
+
+console.log("Absolute Value of Negative Number : ", Math.abs(userAbsolute));
+
+// Q-04
+function diceflow() {
+    let diceNum = Math.floor(Math.random() * 6) + 1;
+    console.log("Random Dice Value :", diceNum);
+}
+//Q-05
+function coinValue() {
+    let coinNum = Math.floor(Math.random() * 2)
+    let coinSide;
+    if (coinNum == 0) {
+        coinSide = "Head"
+    } else {
+        coinSide = "Tail"
+    }
+    console.log("The Value of Coin is :", coinSide);
+
+}
+
+//Q-06
+
+function randomValue() {
+    let randomNum = Math.floor(Math.random() * 100) + 1
+    console.log(randomNum);
+}
+
+//Q-07
+
+let userWeight = prompt("Enter your weight in kilogram :")
+let wieghtOf = parseFloat(userWeight)
+document.write(wieghtOf)
+
+//Q-08
+
+let secretNumber = Math.floor(Math.random() * 10) + 1;
+let userEnter = +prompt("Enter a number between 1 and 10:");
+
+if (userEnter === secretNumber) {
+    alert("Congratulations! You Win!!");
+} else {
+    alert("Sorry! The secret number was " + secretNumber);
+}
